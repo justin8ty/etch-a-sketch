5739565b07883c7d127d12b52a3e537e6d9987f8
Tan Jin Yi <rayneedust@gmail.com>
2022-06-07 20:56:59 +0800

    Implement buttons, barebones CSS, size options
    Finally, a fully working model!

--- README.md
+++ README.md
@@ -17,6 +17,10 @@ This project is designed as part of **The Odin Project's** wonderful Web Develop
 Below are a collection of resources which helped improve my skills :sparkles: while building the project:
 
 - [html - Creating a 16x16 grid using JavaScript - Stack Overflow](https://stackoverflow.com/questions/57550082/creating-a-16x16-grid-using-javascript)
+- [CSS Grid Layout: The Fr Unit | DigitalOcean](https://www.digitalocean.com/community/tutorials/css-css-grid-layout-fr-unit)
+- [CSS grid-template property](https://www.w3schools.com/cssref/pr_grid-template.asp)
+- [Colors HSL](https://www.w3schools.com/colors/colors_hsl.asp)
+- [html - Get the input value after pressing submit button in JavaScript - Stack Overflow](https://stackoverflow.com/questions/56923127/get-the-input-value-after-pressing-submit-button-in-javascript)
 
 ### Non-Technical
 
--- script.js
+++ script.js
@@ -1,13 +1,71 @@
-const drawGrid = document.getElementById("drawGrid");
+//Defaults//
 
-function makeRows(rows, cols) {
-  drawGrid.style.setProperty("--grid-rows", rows);
-  drawGrid.style.setProperty("--grid-cols", cols);
-  for (c = 0; c < rows * cols; c++) {
-    let cell = document.createElement("div");
-    cell.innerText = c + 1;
-    drawGrid.appendChild(cell).className = "grid-item";
+const resetColor = "gainsboro";
+let color = "black";
+let sizeInput = 16;
+
+// Main
+
+function setupGrid(sizeInput) {
+  const sketchBoard = document.querySelector(".sketchBoard");
+
+  sketchBoard.style.gridTemplateColumns = `repeat(${sizeInput}, 1fr)`;
+  sketchBoard.style.gridTemplateRows = `repeat(${sizeInput}, 1fr)`;
+
+  for (let i = 0; i < sizeInput * sizeInput; i++) {
+    const grid = document.createElement("div");
+    grid.classList.add("pixel");
+    grid.addEventListener("mouseover", colorGrid);
+    sketchBoard.appendChild(grid);
   }
 }
 
-makeRows(16, 16);
+setupGrid(sizeInput);
+
+// Option listeners
+
+const blackBtn = document.querySelector(".blackBtn");
+const rainbowBtn = document.querySelector(".rainbowBtn");
+const eraserBtn = document.querySelector(".eraserBtn");
+
+blackBtn.onclick = () => changeColor("black");
+rainbowBtn.onclick = () => changeColor("rainbow");
+eraserBtn.onclick = () => changeColor("gainsboro");
+
+const resetBtn = document.querySelector(".resetBtn");
+resetBtn.onclick = () => resetGrid();
+
+const submitBtn = document.querySelector(".submitBtn");
+submitBtn.addEventListener("click", changeSize);
+
+// Option functions
+
+function colorGrid() {
+  if (color === "rainbow") {
+    this.style.backgroundColor = `hsl(${Math.random() * 360}, 75%, 50%`;
+  } else {
+    this.style.backgroundColor = color;
+  }
+}
+
+function changeColor(colorChoice) {
+  color = colorChoice;
+}
+
+function changeSize() {
+  sizeInput = document.querySelector(".sizeInput").value;
+  console.log(sizeInput);
+
+  if (sizeInput >= 2 && sizeInput <= 98) {
+    resetGrid();
+    setupGrid(sizeInput);
+  } else {
+    alert("Choose an input between 2 and 100");
+  }
+}
+
+function resetGrid() {
+  let sketchBoard = document.querySelector(".sketchBoard");
+  let wipeGrid = sketchBoard.querySelectorAll("div");
+  wipeGrid.forEach((div) => (div.style.backgroundColor = resetColor));
+}
